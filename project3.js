412a6f352a2cdebf5fed7cc39f43903202674590
cnle-coder <cnle5801@gmail.com>
2026-02-14 10:37:49 -0600

    Added a refresh function when mouse is pressed

--- project3.js
+++ project3.js
@@ -1,10 +1,17 @@
 function setup() {
   createCanvas(400, 400);
   background(100);
+  
 }
 
 function draw() {
   
   noStroke()
   fill(255);
-  ellipse(mouseX, mouseY, 1000, 10)
+  ellipse(mouseX, mouseY, 1, 1)
+}
+
+function mousePressed()
+{
+    background(100);
+  }
